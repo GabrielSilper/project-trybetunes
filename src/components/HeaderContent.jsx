ff048bd1777a69452aeebc2977be6cefd0542758
Gabriel Silper <2019003726@ifam.edu.br>
2023-01-02 21:44:41 -0400

    Antes do react icons

--- src/components/HeaderContent.jsx
+++ src/components/HeaderContent.jsx
@@ -1,14 +1,44 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class HeaderContent extends Component {
   render() {
     const { user } = this.props;
     const { name } = user;
     return (
-      <div>
+      <nav className="HeaderContent">
+        <ul className="nav-links">
+          <li>
+            <Link
+              to="/search"
+              data-testid="link-to-search"
+              className="link-navbar"
+            >
+              Pesquisar
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/favorites"
+              data-testid="link-to-favorites"
+              className="link-navbar"
+            >
+              Suas Músicas
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/profile"
+              data-testid="link-to-profile"
+              className="link-navbar"
+            >
+              Perfil
+            </Link>
+          </li>
+        </ul>
         <p data-testid="header-user-name">{name}</p>
-      </div>
+      </nav>
     );
   }
 }
